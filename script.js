1632b45f7b0147a7eb34190a22e7eb59376a6ced
ratnikovb <ratnikovb@gmail.com>
2016-04-28 06:43:41 +0300

    2804,643

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+
+    //Smooth scroll
+    $('a[href^="#"]').click(function () {
+        var el = $(this).attr('href');
+        $('body').animate({
+            scrollTop: $(el).offset().top
+        }, 2000);
+        return false;
+    });
+    //Modal window
     $(function () {
         function hideModal(modalwindow) {
             $(modalwindow).modal('hide');
@@ -13,24 +23,24 @@ $(document).ready(function () {
 
     //   spincrement  Initialization
 
-  var show = true;
+    var show = true;
     var countbox = "#facts";
-    $(window).on("scroll load resize", function(){
+    $(window).on("scroll load resize", function () {
 
-        if(!show) return false;                   // Отменяем показ анимации, если она уже была выполнена
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
 
-        var w_top = $(window).scrollTop();        // Количество пикселей на которое была прокручена страница
-        var e_top = $(countbox).offset().top;     // Расстояние от блока со счетчиками до верха всего документа
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
 
-        var w_height = $(window).height();        // Высота окна браузера
-        var d_height = $(document).height();      // Высота всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
 
         var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
 
-        if(w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height){
+        if (w_top + 200 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
             $(".spincrement").spincrement({
-                thousandSeparator: "",
-                duration: 12000
+                thousandSeparator: ""
+                , duration: 12000
             });
 
             show = false;
@@ -38,39 +48,45 @@ $(document).ready(function () {
     });
     //fitText Initialization
 
-    $("#resp_h2").fitText(0.7, { minFontSize: '20px', maxFontSize: '70px' });
-    $("#resp_h1").fitText(0.7, { minFontSize: '20px', maxFontSize: '150px' });
+    $("#resp_h2").fitText(0.7, {
+        minFontSize: '20px'
+        , maxFontSize: '70px'
+    });
+    $("#resp_h1").fitText(0.7, {
+        minFontSize: '20px'
+        , maxFontSize: '150px'
+    });
+
 
     // accordeon
 
+    function openFirstPanel() {
+        $('.acc_content:first').slideDown().addClass('active');
+        $('.acc_panel:first').children('.fa-angle-down').toggleClass('fa-rotate-180');
+    }
 
-function openFirstPanel() {
-    $('.acc_content:first').slideDown().addClass('active');
-    $('.acc_panel:first').children('.fa-angle-down').toggleClass('fa-rotate-180');
-}
-
-(function ($) {
+    (function ($) {
         $('.acc_content').hide();
         var allPanels = $('.acc_content').hide();
 
-    openFirstPanel();
+        openFirstPanel();
 
-    $('.acc_panel').click(function () {
-        $this = $(this);
-        $target = $this.next();
+        $('.acc_panel').click(function () {
+            $this = $(this);
+            $target = $this.next();
 
 
-        if ($target.hasClass('active')) {
-            $target.removeClass('active').slideUp();
-            $this.children('.fa-angle-down').removeClass('fa-rotate-180');
-        } else {
-            allPanels.removeClass('active').slideUp();
-            $target.addClass('active').slideDown();
-            $this.children('.fa-angle-down').addClass('fa-rotate-180');
-        }
+            if ($target.hasClass('active')) {
+                $target.removeClass('active').slideUp();
+                $this.children('.fa-angle-down').removeClass('fa-rotate-180');
+            } else {
+                allPanels.removeClass('active').slideUp();
+                $target.addClass('active').slideDown();
+                $this.children('.fa-angle-down').addClass('fa-rotate-180');
+            }
 
-        return false;
-    });
+            return false;
+        });
 
-})(jQuery);
+    })(jQuery);
 });
